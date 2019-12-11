45051da72351b9b6e2933dcdc2019c37faf374ba
SortingLee <47915704+SortingLee@users.noreply.github.com>
2019-12-11 20:43:52 +0900

    Add files via upload
    수정내용
    1) /home/camper에 휴지통 디렉토리가 없으면 생성
    2) 삭제 명령어 (ex. rm ./sysprog)를 token으로 나누어 arglist 구성.
    3) execvp로 arglist 실행
    4) 삭제할 파일의 디렉토리 위치(pwd)를 얻어냄
    
    수정필요사항
    segmentation fault..
    ~&(trash가 올바르게 동작한다)같은 명령어는 exec함수로 처리 가능?
    삭제파일 복사(숨김파일? pipe?)
    복원
    완전삭제

---
+++ sysprog.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<dirent.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
+ino_t getInode(char *);
+void dirPath(ino_t);
+void subdirPath(ino_t, char *, int);
+
+char *path = "/";
+
+///segmentation fault.. let code fix
+int main(int argc, char *argv[]) {
+
+	DIR *dir_info;
+	struct dirent *dir_entry;
+	int flag = -1;
+
+	dir_info = opendir("/home/camper");
+
+	if (dir_info == NULL)
+		perror("dir open");
+
+	while (dir_entry = readdir(dir_info)) {
+		if (strcmp(dir_entry->d_name, "trash") == 0) {
+			flag = 1;
+
+			break;
+		}
+	}
+
+	if (flag == -1) {
+		if (mkdir("/home/camper/trash", 0755) == -1) {
+			perror("create error");
+
+			exit(1);
+		}
+	}
+	closedir(dir_info);
+
+	char *token;
+	char *delimiter = " ";
+	char *arglist[BUFSIZ];
+	char argString[BUFSIZ];
+
+	int idx = 0;
+
+	fgets(argString, BUFSIZ, stdin);
+	token = strtok(token, delimiter);
+	while (token != NULL) {
+		arglist[idx++] = token;
+
+		token = strtok(NULL, delimiter);
+	}
+	dirPath(getInode("."));		//pwd얻기
+
+	arglist[idx] = NULL;		//arglist 저장
+	execvp(*arglist, arglist);	//arglist 실행
+
+	return 0;
+}
+
+ino_t getInode(char *filename) {
+	struct stat info;
+
+	if (stat(filename, &info) == -1) {
+		perror(filename);
+
+		exit(1);
+	}
+
+	return info.st_ino;
+}
+
+void dirPath(ino_t st_ino) {
+	ino_t inode;
+	char locate[BUFSIZ];
+
+	if (getInode("..") != st_ino) {
+		chdir("..");
+		subdirPath(st_ino, locate, BUFSIZ);
+
+		inode = getInode(".");
+
+		dirPath(inode);
+		strcat(path, locate);
+	}
+}
+
+void subdirPath(ino_t st_ino, char *string, int length) {
+	DIR *dir_ptr;
+	struct dirent *direntp;
+
+	dir_ptr = opendir(".");
+
+	if (dir_ptr == NULL) {
+		perror(".");
+
+		exit(1);
+	}
+
+	while ((direntp = readdir(dir_ptr)) != NULL) {
+		if (direntp->d_ino == st_ino) {
+			strncpy(string, direntp->d_name, length);
+			string[length - 1] = '\0';
+
+			closedir(dir_ptr);
+
+			return;
+		}
+	}
+
+	printf("dont found");
+	exit(1);
+}
