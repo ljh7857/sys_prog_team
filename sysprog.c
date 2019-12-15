d1089aff2f1c8e0dc63d2dae18ad83bcc4e8bea1
SortingLee <47915704+SortingLee@users.noreply.github.com>
2019-12-16 05:07:03 +0900

    Add files via upload
    SIGNAL을 추가해 main 전체를 while문으로 감쌈. 분명 이렇게하면 fork하지 않아도 exec호출을 반복적으로 할 수 있었는데..
    무슨 이유인지 갑자기 안됨ㅋㅋ(반복문 안에 fork()가 들어가있으면.. process들이 너무 많아짐. (fork bomb) 최대한 fork를 줄이는 고민이 필요할 듯.)
    
    다른 issue들은 C파일안에 주석처리 해놓았음.

--- sysprog.c
+++ sysprog.c
@@ -4,92 +4,153 @@
 #include<unistd.h>
 #include<fcntl.h>
 #include<dirent.h>
+#include<termios.h>
+#include<signal.h>
+#include<ctype.h>
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<sys/wait.h>
+
+#define SLEEPTIME 2
+#define MAXTRIES 3
+#define CAMPER "/home/camper"
+#define TRASHPATH "/home/camper/trash"
+#define oops(message,num) {	perror(message); exit(num);	}
 
 ino_t getInode(char *);
 void dirPath(ino_t);
 void subdirPath(ino_t, char *, int);
 
-char path[BUFSIZ];
+void check_the_trash(void);
+void make_arglist(char *);
 
+void tty_mode(int);
+void set_terminal(void);
+void get_response(int);
+int get_char(void);
+
+void handler(int);
+
+char *arglist[BUFSIZ];
+char *filelist[BUFSIZ];		//삭제 file들의 이름을 저장할 배열
+char *path;
+
+/*	Topics to cover	
+		- chdir issue : Can not chdir with exec function. we should handle this 
+		- pwd file issue : Should we Create pwd file to save path of current file? or there are other ways?
+		- Recycle Bin issue : We need to handle emptying the Recycle Bin and deleting files in the Recycle Bin.
+		- empty issue : If an empty command is entered, segmentation fault is triggered.
+*/
 int main(int argc, char *argv[]) {
 
-	DIR *dir_info;
-	struct dirent *dir_entry;
-	int flag = -1;
-
-	dir_info = opendir("/home/camper");
-
-	if (dir_info == NULL)
-		perror("dir open");
-
-	while (dir_entry = readdir(dir_info)) {
-		if (strcmp(dir_entry->d_name, "trash") == 0) {
-			flag = 1;
-
-			break;
-		}
-	}
-
-	if (flag == -1) {
-		if (mkdir("/home/camper/trash", 0755) == -1) {
-			perror("create error");
-
-			exit(1);
-		}
-	}
-	closedir(dir_info);
-
-	int pid;
-
-	if ((pid = fork()) == -1)
-		perror("fork()");
-
-	if (pid > 0) {
-		wait(NULL);
-		//pwd
-		dirPath(getInode("."));
-		printf("current directory path : ");
-		puts(path);
-
-	}
+	puts("Instructions");
+	printf("You can use the [ ~@ ] command to verify that the Recycle Bin is working.\n - If there is no recycle bin, you can create it.\n - There are no additional options\n");
+	puts("");
+	printf("You can use the [ rm ] command to decide whether to use the Recycle Bin or not.\n - There are additional options. \n");
+	puts("");
+	printf("You can use the [ re ] command to restore files from the Recycle Bin.\n - There are no additional options.\n");
+	puts("");
+	printf("Other commands and options are also supported.\n");
+	puts("");
+	printf("You can end the process using SIGNAL");
+	puts("");
 
 
-	else {
-		//execvp
-		char *token;
-		char *delimiter = " ";
-		char *arglist[BUFSIZ];
+	signal(SIGINT, handler);
+	signal(SIGKILL, SIG_DFL);
+	while (1) {
 		char argString[BUFSIZ];
-
 		int idx = 0;
 
-		printf(" Use the rm command with the files to delete : ");
 		fgets(argString, BUFSIZ, stdin);
 		argString[strlen(argString) - 1] = '\0';
-		token = strtok(argString, delimiter);
-		while (token != NULL) {
 
-			arglist[idx++] = token;
+		if (!strcmp(argString, "~@")) 		//is exist the trash directory?
+			check_the_trash();
+		
+		else {
+			make_arglist(argString);
 
-			token = strtok(NULL, delimiter);
+			if (!strcmp(*arglist, "rm")) {		//delete or trash
+				puts("Do you want to delete the file? Or throw it in the trash?\n - delete : d, throw : t");
+
+				char decision;
+				scanf("%c", &decision);
+
+				if (decision == 't') {
+					dirPath(getInode("."));		//path
+
+					//puts(path);			
+					/*
+					path를 pwd를 저장하는 file로 전달{
+						file로는 한줄에 하나씩 보냄.
+						삭제한 file 정보는 file structure array를 만들어 저장. 
+							- filename, filenumber
+						}
+					
+
+					fork()
+					child - move to trash directory.(exec(mv)) if not exist trash directory, print error message and exit
+					parent - wait until child process exit(), delete the file that in current directory(exec(rm))
+					*/
+				}
+				else 
+					execvp(*arglist, arglist);		//delete anyway	
+			}
+			else if (!strcmp(*arglist, "re")) {		//recover
+				//puts(argString);
+				/*
+				pwd file에서 복원할 file의 이름을 file structure array에서 비교하여 해당 file이 있을 시 pwd file에서
+				filecount 숫자번째 줄의 경로를 받아옴.
+				해당 경로에 file mv! (exec(mv))		
+				*/
+			}
+			else									//other options
+				execvp(*arglist, arglist);
 		}
-		arglist[idx] = NULL;
-
-		/*
-		for (int i = 0; i < idx; i++)
-				printf("arglist[%d] : %s\n", i, arglist[i]);
-
-		*/
-
-		execvp(*arglist, arglist);
-
 	}
+
+
 	return 0;
 }
 
+void make_arglist(char *argString) {
+	char *token;
+	char *delimiter = " ";
+	int idx = 0;
+
+	token = strtok(argString, delimiter);
+	while (token != NULL) {
+		arglist[idx++] = token;
+		token = strtok(NULL, delimiter);
+	}
+	arglist[idx] = NULL;
+}
+
+void check_the_trash(void) {
+	DIR *dir_info;
+	struct dirent *dir_entry;
+
+	dir_info = opendir(CAMPER);
+
+	if (dir_info == NULL)
+		oops("open", 1);
+
+	while (dir_entry = readdir(dir_info)) {
+		if (strcmp(dir_entry->d_name, "trash") == 0) {
+			puts("There is Recycle Bin");
+
+			return;
+		}
+	}
+
+	tty_mode(0);
+	set_terminal();
+	get_response(MAXTRIES);
+	tty_mode(1);
+
+	closedir(dir_info);
+}
 
 
 ino_t getInode(char *filename) {
@@ -145,5 +206,77 @@ void subdirPath(ino_t st_ino, char *string, int length) {
 	}
 
 	printf("dont found");
+	exit(1);
+}
+
+void tty_mode(int how) {
+	static struct termios original_mode;
+	static int original_flags;
+
+	if (how == 0) {
+		tcgetattr(0, &original_mode);
+		original_flags = fcntl(0, F_GETFL);
+	}
+	else {
+		tcsetattr(0, TCSANOW, &original_mode);
+		original_flags = fcntl(0, F_SETFL, original_flags);
+	}
+}
+
+void set_terminal(void) {
+	struct termios ttystate;
+
+	tcgetattr(0, &ttystate);
+	ttystate.c_lflag &= ~ICANON;
+	ttystate.c_lflag &= ~ECHO;
+	ttystate.c_cc[VMIN] = 1;
+
+	tcsetattr(0, TCSANOW, &ttystate);
+}
+
+void get_response(int tries) {
+	char response;
+
+	puts("There is no Recycle bin. Do you want to create it? y/n");
+	response = tolower(get_char());
+
+	switch (response) {
+	case 'y':
+		mkdir(TRASHPATH, 0755);
+		puts("You have successfully created Recycle Bin.");
+
+		return;
+
+	case 'n':
+		puts("Do not create Recycle Bin");
+
+		return;
+	}
+	//시간에 따른 보안상의 이유로 종료도 추가하면 좋을 듯.
+}
+
+int get_char(void) {
+	int ch;
+	int count = 0;
+
+	while (((ch = getchar()) != EOF)) {
+		if (strchr("yYnN", ch))
+			return ch;
+		else
+			puts("you can only enter y or n in any case\n\t-\tIt doesn't matter if it's capital letter or not.");
+		
+		if (++count >= MAXTRIES) {
+			puts("Shut down for security reasons.");
+
+			tty_mode(1);
+			exit(1);
+		}
+	}
+}
+
+void handler(int signum) {
+	//tty_mode(1);
+	puts("shutdown");
+
 	exit(1);
 }
